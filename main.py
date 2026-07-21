8549bc9705b99311bb5207835cb6453a4bc5039a
OUSSAMA HAMDANE <159944691+hamdane1548@users.noreply.github.com>
2026-07-21 15:13:55 +0100

    feat(app): integrate AI agent into application entry point

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from Infrastructure.db.Postgres import PostgressDatabaseConnector
+from Infrastructure.Query_faker import queryDb
+from AiAgent.ReAct_db import ReAct
+from Infrastructure.AgentConf.AgentEnv import AgentEnv
+from loguru import logger
 def main():
-    print("Hello from dataquery-ai!")
-
+   ReAct.CreatetheAgent()
 
 if __name__ == "__main__":
     main()
